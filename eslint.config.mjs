51116fa92bc38e9524efd6a62f0f7cc444c34315
lukasf96 <lukasfock96@gmail.com>
2026-04-20 09:50:58 +0200

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "dist/**",
+      "logs/**",
+      "node_modules/**",
+      "pokelike-source-files/**",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,mjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.es2024,
+        ...globals.node,
+        ...globals.nodeBuiltin,
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  }
+);
